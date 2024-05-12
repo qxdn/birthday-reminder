4c10202f2cbb365182303675d7b6225eebb7e000
Xu Qian <1464238196@qq.com>
2024-05-13 02:59:02 +0800

    [feat] fix annotation in spring native

--- src/main/java/org/qxdn/birthdayreminder/annotation/RoleAdmin.java
+++ src/main/java/org/qxdn/birthdayreminder/annotation/RoleAdmin.java
@@ -1,4 +1,10 @@
 package org.qxdn.birthdayreminder.annotation;
 
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface RoleAdmin {
 }
