3b45c20e6403f0400d4b6ecaa83549eb3fefae60
Xu Qian <1464238196@qq.com>
2024-10-01 08:50:43 +0800

    update config

--- config/config.go
+++ config/config.go
@@ -10,9 +10,17 @@ type Config struct {
 	DB  DB  `yaml:"db"`
 }
 
-var config *Config = &Config{}
+type ApplicationConfig struct {
+	Config      *Config      // 配置
+	ConfigViper *viper.Viper // viper
+}
 
-func ReadConfig() *Config {
+// 配置
+var config *ApplicationConfig = &ApplicationConfig{
+	Config: &Config{},
+}
+
+func ReadConfig() *ApplicationConfig {
 	// 配置文件名
 	viper.SetConfigName("config")
 	// 配置路径
@@ -24,16 +32,15 @@ func ReadConfig() *Config {
 		panic(err)
 	}
 	// 解析配置
-	if err := viper.Unmarshal(config); err != nil {
+	if err := viper.Unmarshal(config.Config); err != nil {
 		panic(err)
 	}
 	// 监听配置变化
 	viper.OnConfigChange(func(e fsnotify.Event) {
-		if err := viper.Unmarshal(config); err != nil {
+		if err := viper.Unmarshal(config.Config); err != nil {
 			panic(err)
 		}
 	})
 	viper.WatchConfig()
-
 	return config
 }
